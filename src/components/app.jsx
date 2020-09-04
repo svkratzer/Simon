e8fe33ce34d10a9490a8a7ecaa4b48efca05c9d2
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-04 18:39:48 -0400

    Render ButtonsContainer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import Button from './button'
+import ButtonsContainer from './buttons/buttons_container'
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <div id="buttons-container">
-          <Button color="Red" />
-          <Button color="Green" />
-          <Button color="Blue" />
-          <Button color="Yellow" />
-        </div>
-      </div>
+      <ButtonsContainer />
     );
   }
 }
