6f707d9100f4e0f323d6279f12a70b243dcc61e9
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-04 18:43:12 -0400

    Change color names

--- src/components/buttons/buttons_container.jsx
+++ src/components/buttons/buttons_container.jsx
@@ -3,9 +3,9 @@ import Button from './button'
 
 export default () => (
     <div id="buttons-container">
-      <Button color="Red" />
-      <Button color="Green" />
-      <Button color="Blue" />
-      <Button color="Yellow" />
+      <Button color="red" />
+      <Button color="green" />
+      <Button color="blue" />
+      <Button color="yellow" />
     </div>
 )
