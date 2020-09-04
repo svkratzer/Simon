d81235b41b2bca3ef1155225b7d94df48bbc4e54
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-04 18:33:20 -0400

    Render bubbles

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import Button from './button'
 
 export default () => (
-  <div>React is up.</div>
+  <div>
+    <div id="buttons-container">
+      <Button color="Red"/>
+      <Button color="Green"/>
+      <Button color="Blue"/>
+      <Button color="Yellow"/>
+    </div>
+  </div>
 )
