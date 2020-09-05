890e4b86165136189d0631732d9a0c06be6255f0
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-04 23:44:48 -0400

    Add logic for keeping track of rounds and checking to see if the sequence is correct

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,15 +7,20 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      gameOver: false,
       playersTurn: false,
       correctSequence: [],
       inputSequence: [],
+      round: 0,
       currentScore: 0,
       highScore: 0
     }
 
     this.correctSequence = [];
     this.inputSequence = [];
+    this.round = 0;
+    this.currentScore = 0;
+    this.highScore = 0;
 
     this._delay = 500
     this.colors = ["green", "red", "yellow", "blue"];
@@ -27,6 +32,7 @@ class App extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     this.playSound = this.playSound.bind(this);
     this.pushNewColor = this.pushNewColor.bind(this);
+    this.reconcileRound = this.reconcileRound.bind(this);
   }
 
   // Plays a different note, depending on the color passed in
@@ -66,11 +72,33 @@ class App extends React.Component {
 
   // Increases the sequences length, and plays through the sequence
   playNextSequence() {
+    // Add the next color
     this.pushNewColor()
     this.setState({ correctSequence: this.correctSequence });
+    // Play the sequence with sounds
     this.correctSequence.forEach((color, i) => {
       setTimeout(() => { this.playNextButton(color) }, this._delay * i);
     });
+    // Increment the round by one
+    this.round += 1
+    this.setState({ round: this.round })
+  }
+
+  // Check to see if the sequences match
+  sequenceIsCorrect() {
+    this.state.inputSequence.forEach((color, i) => {
+      if (color !== this.state.correctSequence[i]) return false;
+    });
+    return true;
+  }
+
+  // Handles game over and increasing current score logic depending on whether the sequence is correct
+  reconcileRound() {
+    if (this.sequenceIsCorrect) {
+
+    } else {
+      this.setState({ gameOver: true })
+    }
   }
 
   handleClick(e) {
@@ -81,7 +109,10 @@ class App extends React.Component {
     this.inputSequence.push(color)
     // Updates state to user input sequence if it's the player's turn
     if (this.state.playersTurn) this.setState({ inputSequence: this.inputSequence });
-
+    // Check to see if the player is done entering the sequence
+    if (this.state.inputSequence.length === this.state.correctSequence.length) {
+      this.reconcileRound();
+    }
     console.log(this.state.inputSequence)
   }
 
@@ -108,6 +139,11 @@ class App extends React.Component {
           <button onClick={(e) => {e.preventDefault(); this.playNextSequence();}}>
             LET'S PLAY
           </button>
+          <div className="stats">
+            <div>
+              Current Round: {this.state.round}
+            </div>
+          </div>
         </section>
       </>
     );
