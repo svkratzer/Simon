93eeb8c38a8b4b1dd2ea584a56ee005e6aa54a32
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-05 17:23:03 -0400

    Style highscores

--- src/components/game_over_modal.jsx
+++ src/components/game_over_modal.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class GameOverModal extends React.Component {
   constructor(props) {
     super(props);
-
     this.handleYes = this.handleYes.bind(this);
   }
 
@@ -14,10 +13,34 @@ class GameOverModal extends React.Component {
   }
 
   render() {
+
     return (
       <div className="modal screen">
         <div className="modal container">
-          <div id="game-over">GAME OVER!</div>
+
+          <div id="highscores-container">
+            <div id="game-over">GAME OVER!</div>
+            { this.props.newHighScore && 
+              <>
+                <div id="new-highscore">
+                  You scored {this.props.highScore}! That's a record!
+                </div>
+                <label>Player Name:&nbsp;
+                  <input id="highscore-name"
+                    placeholder="Enter your name, here."
+                    type="text" 
+                    value={this.props.name}
+                    onChange={this.props.updateName}/>
+                </label>
+              </>
+            }
+
+            <div id="highscores-header">TOP THREE HIGHSCORES</div>
+            <div className="line"></div>
+            <ul id="highscores">
+              {this.props.highScores.slice(3).reverse()}
+            </ul>
+          </div>
 
           <div>
             <div id="play-again">Would you like to play again?</div>
