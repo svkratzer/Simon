c045c54cc94eb12d1152ac33b6a6d2451887987d
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-05 14:23:14 -0400

    Create basic game over modal

---
+++ src/components/game_over_modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class GameOverModal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div className="modal screen">
+      <div className="modal container">
+        <div>GAME OVER!</div>
+        <div>Would you like to play again?</div>
+        <button>heck yes</button>
+        <button>naw dood</button>
+      </div>
+    </div>
+  }
+}
+
+export default GameOverModal;
