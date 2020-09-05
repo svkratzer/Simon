8b33daebbda68807f489e83f6f1b1426b6770799
SVKratzer <samuel.kratzer1894@gmail.com>
2020-09-04 21:32:59 -0400

    Add tones

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import Button from './button'
+import React from 'react';
+import Button from './button';
+import { PolySynth } from 'tone';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
     this.colors = ["green", "red", "yellow", "blue"]
+
+    // Creates a synth and connect it to the main output (users speakers)
+    this.synth = new PolySynth().toDestination();
+
+    // Bind functions
+    this.handleClick = this.handleClick.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  // Plays a different note, depending on the color passed in
+  playSound(color) {
+    const notes = {
+      green: "C4",
+      red: "E4",
+      yellow: "G4",
+      blue: "A4"
+    }
+
+    this.synth.triggerAttackRelease(notes[color], "8n");
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.playSound(e.target.id);
   }
 
   render() {
     
-    // Create an array of Button components by mapping over colors
+    // Create an array of Button components by mapping over this.colors
     const buttons = this.colors.map((color, idx) => (
-      <Button key={idx} color={color}/>
+      <Button onClick={this.handleClick} key={idx} color={color}/>
     ));
 
     return (
